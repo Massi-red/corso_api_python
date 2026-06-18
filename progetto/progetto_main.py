f05d0dc3ecd3d65442c78e96ddea58826e944b95
Massi-red <apd1.mnardinocchi@its4puntozero.it>
2026-06-18 15:29:44 +0000

    ultimo aggiornamento

--- progetto/progetto_main.py
+++ progetto/progetto_main.py
@@ -1,8 +1,18 @@
 from .progetto_db import dbInit
 from fastapi import FastAPI
 from .progetto_prodotti import router as prodotti_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Configuro il CORS per accettare tutto
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],          # Permette l'accesso da qualsiasi sito (Origin)
+    allow_credentials=True,
+    allow_methods=["*"],          # Permette tutti i metodi (GET, POST, PUT, DELETE, ecc.)
+    allow_headers=["*"],          # Permette tutte le intestazioni (Headers)
+)
 
 app.include_router(prodotti_router)
 
